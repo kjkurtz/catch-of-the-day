654a57b5f113bc5a1e0650c92a5c94703d97af4e
Kyle Kurtz <kyle@kurtz.io>
2017-09-30 22:09:08 -0500

    Working app to manage inv and order

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDNa3QYPyRr-vlaeID8WDI3dq4QKjn71Jc",
+  authDomain: "catch-of-the-day-48ee0.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-48ee0.firebaseio.com"
+});
+
+export default base;
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,11 +1,56 @@
 import React from 'react';
+import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
+  constructor() {
+    super();
+    this.renderInventory = this.renderInventory.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e, key) {
+    const fish = this.props.fishes[key];
+    // take a copy of the fisn ahd update
+    const updatedFish = {
+      ...fish,
+      [e.target.name]: e.target.value
+    }
+    this.props.updateFish(key, updatedFish);
+  }
+  renderInventory(key) {
+    const fish = this.props.fishes[key];
+    return (
+      <div className="fish-edit" key={key}>
+        <input type="text" name="name" value={fish.name} placeholder="Fish name" onChange={(e) => this.handleChange(e, key)}/>
+        <input type="text" name="price" value={fish.price} placeholder="Fish price" onChange={(e) => this.handleChange(e, key)}/>
+        <select type="text" name="status" value={fish.status} placeholder="Fish status" onChange={(e) => this.handleChange(e, key)}>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea type="text" name="desc" value={fish.desc} placeholder="Fish desc" onChange={(e) => this.handleChange(e, key)}></textarea>
+        <input type="text" name="image" value={fish.image} placeholder="Fish image" onChange={(e) => this.handleChange(e, key)}/>
+        <button onClick={() => this.props.removeFish(key)}>Remove Fish</button>
+      </div>
+    )
+  }
   render() {
     return (
-      <p>Inventory</p>
+    	<div>
+	      <p>Inventory</p>
+        {Object.keys(this.props.fishes).map(this.renderInventory)}
+	      <AddFishForm addFish={this.props.addFish} />
+	      <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
+      </div>
     )
   }
 }
 
-export default Inventory;
+Inventory.propTypes = {
+  fishes: React.PropTypes.object.isRequired,
+  updateFish: React.PropTypes.func.isRequired,
+  removeFish: React.PropTypes.func.isRequired,  
+  addFish: React.PropTypes.func.isRequired,
+  loadSamples: React.PropTypes.func.isRequired
+};
+
+export default Inventory
