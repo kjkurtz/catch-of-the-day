c94b14eb49b8918e81b4e301fc77ddf4c5a979eb
Kyle Kurtz <kjkurtz@gmail.com>
2018-07-31 21:30:42 -0500

    Lessons 1-4

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,33 +1,17 @@
 import React from 'react';
-import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-	// constructor() {
-	// 	super();
-	// 	this.goToStore = this.goToStore.bind(this);
-	// }
-	goToStore(event) {
-		event.preventDefault();
-		console.log('You changed the url');
-		//first grab the text from the box
-		const storeId = this.storeInput.value;
-		//second going to transition from / to /store/:storeId
-		this.context.router.transitionTo(`/store/${storeId}`);
-	}
-	render() {
-		return (
-			<form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
-				<h2>Please Enter A Store</h2>
-				<input type="text" required placeholder="Store Name"
-					defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
-				<button type="submit">Visit Store </button>
-			</form>
-		)
-	}
-}
 
-StorePicker.contextTypes = {
-	router: React.PropTypes.object
+  render() {
+    return (
+      <form className="store-selector">
+        { /* comment */ }
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store</button>
+      </form>
+    )
+  }
 }
 
 export default StorePicker;
